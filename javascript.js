dd0d1d30eae4fdfaa6ea119af02ccda8248f5793
Justin Doub <justindoub@gmail.com>
2022-04-18 15:02:57 +0900

    Add computerPlay

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function computerPlay() {
+   let number = Math.floor(Math.random() * 3);
+    if ( number == 0) {
+        return "rock";
+    }
+    else if ( number == 1) {
+        return "paper";
+    }
+    else return "scissors";
+}
+
+console.log(computerPlay());
