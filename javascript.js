bae1991fd077c76d2562ca39ce9a767fa1278bf8
Justin Doub <justindoub@gmail.com>
2022-06-09 11:34:29 +0900

    Add scoring system and 5 round game

--- javascript.js
+++ javascript.js
@@ -1,57 +1,96 @@
 //generate a random computer play
 function computerPlay() {
-   let number = Math.floor(Math.random() * 3);
-    if ( number == 0) {
-        return "rock";
-    }
-    else if ( number == 1) {
+    let number = Math.floor(Math.random() * 3);
+     if ( number == 0) {
+         console.log("Computer: rock");
+         return "rock";
+     }
+     else if ( number == 1) {
+        console.log("Computer: paper"); 
         return "paper";
-    }
-    else return "scissors";
+     }
+     else console.log("Computer: scissors");
+        return "scissors";
+ }
+ 
+ //player will choose rock, paper, or scissors
+function playerPlay() {
+    let playerSelection = prompt("What's your play? Rock, paper, or scissors?").toLowerCase();
+    console.log("You: " + playerSelection);
+    return playerSelection;
 }
 
-//player will choose rock, paper, or scissors
-let rock
-let paper
-let scissors
+ //compare computer and human play, add score, and return a result 
+ function playRound() {
+     let playerSelection = playerPlay();
+     let computerSelection = computerPlay();
+     //let computerScore = 0;
+     //let playerScore = 0;
+     if (playerSelection == computerSelection ) {
+         console.log("It's a tie!");
+         console.log("You: " + playerScore + " Computer: " + computerScore);
 
-let playerPlay = prompt("What's your play? Rock, paper, or scissors?").toLowerCase()
+     }
+     else if (playerSelection === "rock" && computerSelection === "paper" ) {
+        console.log("You lose the round! Paper beats rock."); 
+        computerScore++;
+        console.log("You: " + playerScore + " Computer: " + computerScore);
+        return computerScore + 1;
+     }
+     else if (playerSelection === "rock" && computerSelection === "scissors") {
+        console.log("You win the round! Rock beats scissors.");
+        playerScore++; 
+        console.log("You: " + playerScore + " Computer: " + computerScore);
+        return playerScore + 1;
+     }
+     else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("You win the round! Paper beats rock.");
+        playerScore++; 
+        console.log("You: " + playerScore + " Computer: " + computerScore);
+        return playerScore + 1;
+     }
+ 
+     else if (playerSelection === "paper" && computerSelection === "scissors") {
+        console.log("You lose the round! Scissors beat paper.");
+        computerScore++; 
+        console.log("You: " + playerScore + " Computer: " + computerScore);
+        return computerScore + 1;
+     }
+     else if (playerSelection === "scissors" && computerSelection === "rock") {
+        console.log("You lose the round! Rock beats scissors.");
+        computerScore++; 
+        console.log("You: " + playerScore + " Computer: " + computerScore);
+        return computerScore + 1;
+     }
+     else if (playerSelection === "scissors" && computerSelection === "paper" ) {
+        console.log("You win the round! Scissors beat paper.");
+        playerScore++; 
+        console.log("You: " + playerScore + " Computer: " + computerScore);
+        return playerScore + 1;
+     }
+     else console.log("You didn't say rock, paper, or scissors. What kind of scam are you trying to run?");
+        return "You didn't say rock, paper, or scissors. What kind of scam are you trying to run?";
+   }   
+   const playerSelection = playerPlay;
+   const computerSelection = computerPlay();
+   //console.log(playRound(playerSelection, computerSelection));
 
-//compare computer and human plays and return a result
 
-// I think I'm not using .toLowerCase correctly here.
+function game() {
+    //play 5 rounds
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+}
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection ) {
-        return "It's a tie!";
-    }
-    else if (playerSelection === "rock" && computerSelection === "paper" ) {
-        return "You lose! Paper beats rock.";
-    }
-    else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You win! Rock beats scissors.";
-    }
-    else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You win! Paper beats rock.";
-    }
+//scores
+let playerScore = 0;
+let computerScore = 0;
 
-    else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You lose! Scissors beat paper.";
-    }
-    else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You lose! Rock beats scissors.";
-    }
-    else if (playerSelection === "scissors" && computerSelection === "paper" ) {
-        return "You win! Scissors beat paper.";
-    }
-    else return "You didn't say rock, paper, or scissors. What kind of scam are you trying to run?";
-  }   
+game();
 
 //play 5 rounds
-for (let i = 0; i < 5; i++) {
-    // your code here!
- }
-
-  const playerSelection = playerPlay;
-  const computerSelection = computerPlay();
-  console.log(playRound(playerSelection, computerSelection));
+ //for (let i = 0; i < 5; i++) {
+ //    if (i < 5) {(console.log(playRound())) && computerSelection()};
+    
+ //}
